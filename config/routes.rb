7464816317f0333d6b3ee02b66449963a809466e
mikiko.homma <kiku617lenkyun@yahoo.co.jp>
2020-08-06 04:16:36 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
-    root to: 'tasks#index'
-    resources :tasks
-end
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+   root to: 'tasks#index'
+   
+   get 'users/index'
+   get 'users/show'
+   get 'users/new'
+   get 'users/create'
+   
+   get 'login', to: 'sessions#new'
+   post 'login', to: 'sessions#create'
+   delete 'logout', to: 'sessions#destroy'
+
+    
+   get 'signup', to: 'users#new'
+   resources :users, only: [:index, :show, :new, :create]
+   
+   resources :tasks
+ end
